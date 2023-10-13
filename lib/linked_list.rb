c0bf99e05d83068c54fdfd3c1a59508db5eae86f
Yain Porter <yainporter@gmail.com>
2023-10-12 23:53:59 -0700

    Create a #count and #to_string

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -11,7 +11,7 @@ class LinkedList
     if @head == nil
       @head = Node.new(data)
     else
-      # How do I access the next node? I know that I can access the attributes of a node if I pass in that data...
+      # How do I access the next node? I know that I can access the attributes of a node if I pass in that data... DUH!! Use attr_accessor instead of attr_reader!!!
       current_node = @head
       node_2 = Node.new(data)
       current_node.next_node = node_2
@@ -19,11 +19,21 @@ class LinkedList
   end
 
   def count
-
+    if @head == nil
+      count = 0 
+    else
+      count = 1
+      current_node = @head
+      while current_node.next_node != nil
+        current_node = current_node.next_node
+        count += 1
+      end
+    end
+    count
   end
 
   def to_string
-
+    self.head.data
   end
 
 end
